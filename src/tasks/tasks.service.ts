82c1af7becbe5443a0cd5a7e9a9fca5b3458fc4b
Piotr Kowalski <perek.kowalski@gmail.com>
2023-07-25 16:11:16 +0200

    Task ownership and restrictions

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -6,6 +6,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Task } from './task.entity';
 import { Repository } from 'typeorm';
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
+import { User } from 'src/auth/user.entity';
 
 @Injectable()
 export class TasksService {
@@ -14,16 +15,19 @@ export class TasksService {
     private tasksRepository: Repository<Task>,
   ) {}
 
-  async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+  async getTasks(filterDto: GetTasksFilterDto, user: User): Promise<Task[]> {
     const query = this.tasksRepository.createQueryBuilder('task');
     const { status, search } = filterDto;
+    query.where({ user });
+    // const userId = user.id;
+    // query.andWhere('task.userId = :userId', { userId });
 
     if (status) {
       query.andWhere('task.status = :status', { status });
     }
     if (search) {
       query.andWhere(
-        'LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search)',
+        '(LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search))',
         { search: `%{search}%` },
       );
     }
@@ -36,28 +40,35 @@ export class TasksService {
     return await this.tasksRepository.find();
   }
 
-  async getTaskById(id: string): Promise<Task> {
-    const found = await this.tasksRepository.findOneBy({ id: id });
-    if (!found) {
-      throw new NotFoundException(`Task with id ${id} was not found.`);
+  async getTaskById(id: string, user: User): Promise<Task> {
+    const task = await this.tasksRepository.findOneBy({ id: id, user: user });
+    // This works too!
+    // const query = this.tasksRepository.createQueryBuilder('task');
+    // query.where({ user });
+    // query.andWhere('task.id = :id', { id });
+    // const task: Task = await query.getOne();
+    if (!task) {
+      throw new NotFoundException(`Task with ${id} was not found.`);
     }
-    return found;
+    return task;
   }
 
-  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+  async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
     const { title, description } = createTaskDto;
     const task = this.tasksRepository.create({
       title,
       description,
       status: TaskStatus.OPEN,
+      user,
     });
 
     await this.tasksRepository.save(task);
     return task;
   }
 
-  async deleteTask(id: string): Promise<void> {
-    const result = await this.tasksRepository.delete(id);
+  async deleteTask(id: string, user: User): Promise<void> {
+    const result = await this.tasksRepository.delete({ id, user });
+
     if (result.affected === 0) {
       throw new NotFoundException(`Task with id ${id} was not found.`);
     }
@@ -67,9 +78,10 @@ export class TasksService {
   async updateTaskStatus(
     id: string,
     updateTaskStatusDto: UpdateTaskStatusDto,
+    user: User,
   ): Promise<Task> {
     const { status } = updateTaskStatusDto;
-    if (this.getTaskById(id)) {
+    if (await this.getTaskById(id, user)) {
       return await this.tasksRepository.save({
         id: id,
         status: status,
