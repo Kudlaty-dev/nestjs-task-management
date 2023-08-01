41b6dce5ba9bf6d2804796ca1dc03f73772872a1
Piotr Kowalski <perek.kowalski@gmail.com>
2023-08-01 15:15:40 +0200

    Added couple of unit tests for practice purposes

--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -2,20 +2,68 @@ import { Test } from '@nestjs/testing';
 import { TasksService } from './tasks.service';
 import { Repository } from 'typeorm';
 import { Task } from './task.entity';
+import { TasksRepository } from './tasks.repository';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { TaskStatus } from './task-status.enum';
+import { NotFoundException } from '@nestjs/common';
 
-const mockTasksRepository = () => ({});
+const mockTasksRepository = () => ({
+  getTasks: jest.fn(),
+  getTaskById: jest.fn(),
+});
+
+const mockUser = {
+  username: 'Piotr',
+  id: 'someId',
+  password: 'somePassword',
+  tasks: [],
+};
+
 describe('TasksService', () => {
   let tasksService: TasksService;
-  let tasksRepository: Repository<Task>;
+  let tasksRepository; //TasksRepository;
 
   beforeEach(async () => {
-    // Initialize a NestJs module with tasksService and tasksModule
+    // Initialize a NestJs module with tasksService and tasksRepository
     const module = await Test.createTestingModule({
       providers: [
         TasksService,
-        { provide: Repository<Task>, useFactory: mockTasksRepository },
+        { provide: getRepositoryToken(Task), useFactory: mockTasksRepository },
       ],
     }).compile();
     tasksService = module.get(TasksService);
+    tasksRepository = module.get(getRepositoryToken(Task));
+  });
+
+  describe('getTasks', () => {
+    it('calls TasksRepository.getTasks and returns the result', async () => {
+      expect(tasksRepository.getTasks).not.toHaveBeenCalled();
+      // call tasks.Service.getTasks, which shoyld then call the repository's getTasks
+      tasksRepository.getTasks.mockResolvedValue('someValue');
+      const result = await tasksService.getTasks(null, mockUser);
+      expect(tasksRepository.getTasks).toHaveBeenCalled();
+      expect(result).toEqual('someValue');
+    });
+  });
+
+  describe('getTaskById', () => {
+    it('calls TasksRepository.findOne and returns the result', async () => {
+      const mockTask = {
+        title: 'Test title',
+        description: 'Test desc',
+        id: 'someId',
+        status: TaskStatus.OPEN,
+      };
+
+      tasksRepository.getTaskById.mockResolvedValue(mockTask);
+      const result = await tasksService.getTaskById('someId', mockUser);
+      expect(result).toEqual(mockTask);
+    });
+    // it('calls TasksRepository.findOne and handles an error', async () => {
+    //   tasksRepository.getTaskById.mockResolvedValue(null);
+    //   expect(tasksService.getTaskById('someId', mockUser)).rejects.toThrow(
+    //     NotFoundException,
+    //   );
+    // });
   });
 });
